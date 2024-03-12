724e9774f22ca66ab9ca77ba5ad55ef8e16c545e
kikealcocerdz <enrique.alcocer1203@gmail.com>
2024-03-12 11:41:35 +0100

    servidor concurrente v1

--- source/cliente.c
+++ source/cliente.c
@@ -13,12 +13,6 @@ int main() {
   int N_value2 = 3;
   double V_value2[] = {1.0, 2.0, 3.0};
   set_value(key, value1, N_value2, V_value2);
-  int key2 = 2;
-  char value2[] = "Hello Many Times";
-  int N_value22 = 3;
-  double V_value22[] = {1.0, 2.0, 3.0};
-  delete_key(key2);
-  exist(59);
   return 0;  // Indicate successful execution
 }
 
